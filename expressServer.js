f59caf234573dae1c002ce701e535ac0daccea75
David Figueroa <figueroadavid93@gmail.com>
2022-10-31 17:45:55 -0400

    implemented random id generator to only produce 6 digit shortURL

--- expressServer.js
+++ expressServer.js
@@ -11,6 +11,8 @@ const urlDatabase = {
   "9sm5xk": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,6 +28,29 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urlsIndex", templateVars)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urlsNew");
+});
+
+function generateRandomString() {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL)
+    const longURL = req.body.longURL;
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = longURL
+    console.log(urlDatabase);
+    res.send("OK");
 });
 
 app.get("/urls/:id", (req, res) => {
